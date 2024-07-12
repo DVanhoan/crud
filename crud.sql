e77f9d0a57404cd30d49f4aa2da7cf59e860547a
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-07-12 19:06:46 +0700

    1

--- crud.sql
+++ crud.sql
@@ -1,25 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 80035 (8.0.35)
- Source Host           : localhost:3306
- Source Schema         : crud
-
- Target Server Type    : MySQL
- Target Server Version : 80035 (8.0.35)
- File Encoding         : 65001
-
- Date: 12/07/2024 19:05:16
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -30,9 +8,7 @@ CREATE TABLE `user`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of user
--- ----------------------------
+
 INSERT INTO `user` VALUES (1, 'hoan', 'hoan@gmail.com', 'dak nong', '0334084617');
 INSERT INTO `user` VALUES (2, 'nam', 'nam@gmail.com', '', '123344556');
 INSERT INTO `user` VALUES (4, 'hung', 'hung@gmail.com', '', '12321312312');
